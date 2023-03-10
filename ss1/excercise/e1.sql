3f3c70b978a2f37711b380805006a6a175fcc1f3
kendulee41 <kendulee41@gmail.com>
2023-03-10 21:30:51 +0700

    up e2

--- ss1/excercise/e1.sql
+++ ss1/excercise/e1.sql
@@ -1,12 +1,15 @@
 create database student_management;
 use student_management;
+
 create table class(
 id int not null,
-name varchar(50) not null,
+`name` varchar(50) not null,
 primary key(id));
+
+
 create table teacher(
 id int not null,
-name varchar(50) not null,
-age int not null,
+`name` varchar(50) not null,
+age int check (age > 18),
 country varchar(255) not null,
 primary key(id));
