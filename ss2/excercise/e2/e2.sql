bd6aaf71a7e62cce73b60efe0bfea7940bd22872
kendulee41 <kendulee41@gmail.com>
2023-03-13 21:12:35 +0700

    up ss2

---
+++ ss2/excercise/e2/e2.sql
@@ -0,0 +1,30 @@
+create database quanlybanhang;
+use quanlybanhang;
+create table customer(
+	cID int,
+    cName varchar(30) not null,
+    cAge bit not null,
+    primary key(cID)
+);
+create table `order`(
+	oID int,
+    cID int,
+    oDate varchar(20) not null,
+    oTotalPrice int not null,
+    primary key(oID),
+    foreign key(cID) references customer(cID)
+);
+create table product(
+	pID int,
+    pName varchar(30) not null,
+    pPrice int not null,
+    primary key(pID)
+);
+create table orderDetail(
+	pID int,
+    oID int,
+    odQTY varchar(255) not null,
+    primary key(pID, oID),
+    foreign key(oID)references `order`(oID),
+    foreign key(pID)references product(pID)
+);
